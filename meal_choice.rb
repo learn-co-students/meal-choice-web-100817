2d666619ff0e788538b5b93d16a3cd3db2b7550a
cunhasb CUNHA <cunhasb@aol.com>
2017-10-02 15:06:44 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,6 +2,8 @@ require 'pry'
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice(your_choice ="meat"){
+def meal_choice(choice ="meat")
+  choice 
+  binding.pry 
+end
 
-}
